6f310da4a85277c7b43af7deb4c58e1912f0f783
Zeta-Umbrella <khaledsouguir8@gmail.com>
2026-09-06 10:32:03 +0200

    Update dhcp_config.sh

--- config/dhcp_config.sh
+++ config/dhcp_config.sh
@@ -5,3 +5,9 @@ sudo systemctl status kea-dhcp4-server
 sudo mv /etc/kea/kea-dhcp4.conf /etc/kea/kea-dhcp4.conf.bkp
 sudo cat ./kea_conf.json > /etc/kea/kea-dhcp.conf
 sudo systemctl restart kea-dhcp4-server
+sudo systemctl status kea-dhcp4-server
+
+# fichier csv contenant la liste des ip addresses : /var/lib/kea/kea-leases4.csv
+# dhclient permet de demander une adresse ip
+#  https://youtu.be/tXjKPJ5KJUY --> c'est la vidéo youtube de IT - Connect
+
