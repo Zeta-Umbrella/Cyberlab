7acf1b520f310b65f922decf3c9220a294a22e2e
Zeta-Umbrella <khaledsouguir8@gmail.com>
2026-09-06 09:55:09 +0200

    Update dhcp_config.sh

--- config/dhcp_config.sh
+++ config/dhcp_config.sh
@@ -3,3 +3,5 @@
 sudo apt install kea-dhcp4-server -y
 sudo systemctl status kea-dhcp4-server
 sudo mv /etc/kea/kea-dhcp4.conf /etc/kea/kea-dhcp4.conf.bkp
+sudo cat ./kea_conf.json > /etc/kea/kea-dhcp.conf
+sudo systemctl restart kea-dhcp4-server
