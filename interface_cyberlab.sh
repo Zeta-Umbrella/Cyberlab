09e764a7cc59ccf4ce855f0bbfca512d3e1e350f
Zeta-Umbrella <khaledsouguir8@gmail.com>
2026-09-06 16:08:44 +0200

    Update interface_cyberlab.sh

--- interface_cyberlab.sh
+++ interface_cyberlab.sh
@@ -10,12 +10,21 @@ function verify_dev () {
     
   if [[ $dev -eq "cyberlab" ]] then
     echo -e "\nThere is already a device named cyberlab\n"
+    return 1
 
   else if  [[ $dev -n ]] then
     echo -e "\nDevice seem to be prepared for configuration\n"
-
+    return 0
 }
 
-  
-    
+function set_dev () {
+  mac=$(ip link show $dev | awk '/link\/ether/ {print $2}')
+  echo -e "[Match]\nMACAddress=$mac\n[Link]\nName=$dev" | cat > /etc/systemd/network/10-lan0.link
+  sudo systemctl restart systemd-udevd
 
+  if [[ $(ip link show $dev | awk '{print $2}')
+  
+  
+  
+
+  
