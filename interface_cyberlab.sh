1e7f8cce6cec4c71a0182592534706fb102a1e68
Zeta-Umbrella <khaledsouguir8@gmail.com>
2026-09-07 06:45:49 +0200

    Update interface_cyberlab.sh
    corrected syntaxe (`fi` and `;`) and end function set_dev_name

--- interface_cyberlab.sh
+++ interface_cyberlab.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
-function verify_dev () {
+function verify_dev_name () {
 
   dev=$(ip link | grep "ens37\|eth1\|cyberlab" | awk -F: '{print $2}')
   
-  if [[ $dev -z ]] then
+  if [[ $dev -z ]] ; then
     echo -e "\nThere is no device dedicated to the cyberlab LAN segment\n"
     return 1
-    
-  if [[ $dev -eq "cyberlab" ]] then
+  fi
+  
+  if [[ $dev -eq "cyberlab" ]] ; then
     echo -e "\nThere is already a device named cyberlab\n"
     return 1
-
-  else if  [[ $dev -n ]] then
+  fi
+  
+  else if  [[ $dev -n ]] ; then
     echo -e "\nDevice seem to be prepared for configuration\n"
     return 0
+  fi
 }
 
-function set_dev () {
+
+function set_dev_name () {
 
   mac=$(ip link show $dev | awk '/link\/ether/ {print $2}')
   
@@ -26,7 +30,14 @@ function set_dev () {
   sudo systemctl restart systemd-udevd
   
 # verify if $dev is corresponding to the name cyberlab
-  if [[ $(ip link show $dev | awk '{print $2}')
+  if [[ $(ip link show $dev | awk '{print $2}' | grep cyberlab | sed 's/://g' | sed 's/\n//g' ) -eq 'cyberlab' ]] ; then
+    echo -e "The device is now set to 'cyberlab'"
+    return 0
+  fi
+
+
+
+    
   
   
   
